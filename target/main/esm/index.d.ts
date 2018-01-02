bc6b6a74941dc220bf8b92549b61b998c0a1d4f5
Bas van Dijk <bas@usto.nl>
2018-01-02 22:21:51 +0100

    update

--- target/main/esm/index.d.ts
+++ target/main/esm/index.d.ts
@@ -1,2 +1,19 @@
-declare const _default: "./domusto-marantz";
-export default _default;
+/**
+ * GPIO plugin for DOMUSTO
+ * @author Bas van Dijk
+ * @version 0.0.1
+ *
+ * @class DomustoMarantz
+ * @extends {DomustoPlugin}
+ */
+declare class DomustoMarantz {
+    domustoPlugin: any;
+    /**
+     * Creates an instance of DomustoMarantz.
+     * @param {any} Plugin configuration as defined in the config.js file
+     * @memberof DomustoMarantz
+     */
+    constructor(pluginConfiguration: any, domustoPlugin: any);
+    outputCommand(device: any, command: any, onSucces: any): void;
+}
+export default DomustoMarantz;
